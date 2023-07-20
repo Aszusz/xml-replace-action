838e6167938bf46a1d9aa0f7d0bc9bdf7beb707e
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2023-07-20 23:18:56 +0200

    Update for node 16

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const core = require('@actions/core');
-const xpath = require('xpath');
-const DOMParser = require('xmldom').DOMParser;
-const XMLSerializer = require('xmldom').XMLSerializer;
-const fs = require('fs');
+import { getInput, setFailed } from '@actions/core';
+import { select } from 'xpath';
+import { DOMParser, XMLSerializer } from '@xmldom/xmldom';
+import { readFileSync, writeFileSync } from 'fs';
 
 try {
-    let filePath = core.getInput('filepath', {required: true});
-    let xpathString = core.getInput('xpath', {required: true});
-    let replaceString = core.getInput('replace');
+    let filePath = getInput('filepath', {required: true});
+    let xpathString = getInput('xpath', {required: true});
+    let replaceString = getInput('replace');
 
     writeFile(filePath, xpathString, replaceString);
 } catch (error) {
     console.log(error)
-    core.setFailed(error.message);
+    setFailed(error.message);
 }
 
 function writeFile(filePath, xpathString, replaceString) {
-    const content = fs.readFileSync(filePath, 'utf8');
+    const content = readFileSync(filePath, 'utf8');
     const document = new DOMParser().parseFromString(content);
 
-    const nodes = xpath.select(xpathString, document);
+    const nodes = select(xpathString, document);
     if (nodes.length === 0) {
-        core.setFailed("No matching xml nodes found");
+        setFailed("No matching xml nodes found");
     } else {
         for (const node of nodes) {
             console.log("Setting xml value at " + getNodePath(node));
@@ -31,7 +30,7 @@ function writeFile(filePath, xpathString, replaceString) {
                 node.textContent = replaceString;
             }
         }
-        fs.writeFileSync(filePath, new XMLSerializer().serializeToString(document));
+        writeFileSync(filePath, new XMLSerializer().serializeToString(document));
     }
 }
 
